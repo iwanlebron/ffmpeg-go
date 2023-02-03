eeba8fb26dffe6a54c3ba2082b3733f31d14ab4f
张国庆 <iwanlebron@foxmail.com>
2023-02-03 17:04:26 +0800

    init

---
+++ examples/example_test.go
@@ -0,0 +1,83 @@
+package examples
+
+import (
+	"testing"
+	
+	"github.com/disintegration/imaging"
+	"github.com/stretchr/testify/assert"
+	
+	ffmpeg "github.com/iwanlebron/ffmpeg-go"
+)
+
+//
+// More simple examples please refer to ffmpeg_test.go
+//
+
+func TestExampleStream(t *testing.T) {
+	ExampleStream("./sample_data/in1.mp4", "./sample_data/out1.mp4", false)
+}
+
+func TestExampleReadFrameAsJpeg(t *testing.T) {
+	reader := ExampleReadFrameAsJpeg("./sample_data/in1.mp4", 5)
+	img, err := imaging.Decode(reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = imaging.Save(img, "./sample_data/out1.jpeg")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestExampleShowProgress(t *testing.T) {
+	ExampleShowProgress("./sample_data/in1.mp4", "./sample_data/out2.mp4")
+}
+
+func TestExampleChangeCodec(t *testing.T) {
+	err := ffmpeg.Input("./sample_data/in1.mp4").
+		Output("./sample_data/out1.mp4", ffmpeg.KwArgs{"c:v": "libx265"}).
+		OverWriteOutput().ErrorToStdOut().Run()
+	assert.Nil(t, err)
+}
+
+func TestExampleCutVideo(t *testing.T) {
+	err := ffmpeg.Input("./sample_data/in1.mp4", ffmpeg.KwArgs{"ss": 1}).
+		Output("./sample_data/out1.mp4", ffmpeg.KwArgs{"t": 1}).OverWriteOutput().Run()
+	assert.Nil(t, err)
+}
+
+func TestExampleScaleVideo(t *testing.T) {
+	err := ffmpeg.Input("./sample_data/in1.mp4").
+		Output("./sample_data/out1.mp4", ffmpeg.KwArgs{"vf": "scale=w=480:h=240"}).
+		OverWriteOutput().ErrorToStdOut().Run()
+	assert.Nil(t, err)
+}
+
+func TestExampleAddWatermark(t *testing.T) {
+	// show watermark with size 64:-1 in the top left corner after seconds 1
+	overlay := ffmpeg.Input("./sample_data/overlay.png").Filter("scale", ffmpeg.Args{"64:-1"})
+	err := ffmpeg.Filter(
+		[]*ffmpeg.Stream{
+			ffmpeg.Input("./sample_data/in1.mp4"),
+			overlay,
+		}, "overlay", ffmpeg.Args{"10:10"}, ffmpeg.KwArgs{"enable": "gte(t,1)"}).
+		Output("./sample_data/out1.mp4").OverWriteOutput().ErrorToStdOut().Run()
+	assert.Nil(t, err)
+}
+
+func TestExampleCutVideoForGif(t *testing.T) {
+	err := ffmpeg.Input("./sample_data/in1.mp4", ffmpeg.KwArgs{"ss": "1"}).
+		Output("./sample_data/out1.gif", ffmpeg.KwArgs{"s": "320x240", "pix_fmt": "rgb24", "t": "3", "r": "3"}).
+		OverWriteOutput().ErrorToStdOut().Run()
+	assert.Nil(t, err)
+}
+
+func TestExampleMultipleOutput(t *testing.T) {
+	input := ffmpeg.Input("./sample_data/in1.mp4").Split()
+	out1 := input.Get("0").Filter("scale", ffmpeg.Args{"1920:-1"}).
+		Output("./sample_data/1920.mp4", ffmpeg.KwArgs{"b:v": "5000k"})
+	out2 := input.Get("1").Filter("scale", ffmpeg.Args{"1280:-1"}).
+		Output("./sample_data/1280.mp4", ffmpeg.KwArgs{"b:v": "2800k"})
+	err := ffmpeg.MergeOutputs(out1, out2).OverWriteOutput().ErrorToStdOut().Run()
+	assert.Nil(t, err)
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/iwanlebron/ffmpeg-go
+
+go 1.19
+
+require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/stretchr/testify v1.5.1
+	github.com/u2takey/go-utils v0.3.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
